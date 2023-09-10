f0de270833ce7405a4a0212632ce1f1282a04d6c
BSevault <b.sevault@gmail.com>
2023-09-10 16:35:19 +0200

    perf optimization

--- script/script.js
+++ script/script.js
@@ -1,14 +1,19 @@
-<<<<<<< HEAD:script/script.js
 const originDateTime = localStorage.getItem('originDateTime');
 
 function calculateDuration(date1, date2) {
     const diff = date1.diff(date2, ['years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds']).values;
-    diff.seconds = Math.round(diff.seconds);
+    diff.seconds = Math.trunc(diff.seconds);
     return diff;
 }
 
-function generateDurationString(durationObject) {
-    const { years, months, weeks, days, hours, minutes, seconds } = durationObject;
+function calculateWeekDuration(date1, date2) {
+    const diff = date1.diff(date2, ['weeks', 'days']).values;
+    diff.days = Math.trunc(diff.days);
+    return diff;
+}
+
+function generateDurationString(durationObject, weekDate) {
+    const { years, months, weeks, days, hours, minutes } = durationObject;
 
     const yearsArray = ["an", "ans"];
     const weeksArray = ["semaine", "semaines"];
@@ -17,13 +22,19 @@ function generateDurationString(durationObject) {
 
     const yearsString = years === 1 ? `${years} ${yearsArray[0]} ` : years > 1 ? `${years} ${yearsArray[1]} ` : '';
 
+    const monthsString = months == null ? '' : `${months} mois `;
+
     const weeksString = weeks === 1 ? `${weeks} ${weeksArray[0]} ` : weeks > 1 ? `${weeks} ${weeksArray[1]} ` : '';
 
     const daysString = days === 1 ? `${days} ${daysArray[0]} ` : days > 1 ? `${days} ${daysArray[1]} ` : '';
 
     const hoursString = hours === 1 ? `${hours} ${hoursArray[0]} ` : hours > 1 ? `${hours} ${hoursArray[1]} ` : '';
 
-    const durationString = `${yearsString}${months} mois ${weeksString}${daysString}${hoursString}${minutes} minutes ${seconds} secondes`;
+    const minutesString = minutes == null ? '' : `${minutes} minutes `;
+
+    const string = `${yearsString}${monthsString}${weeksString}${daysString}${hoursString}${minutesString}`;
+
+    const durationString = !weekDate ? string : 'ou ' + string;
 
     return durationString;
 }
@@ -37,12 +48,29 @@ if (!originDateTime) {
     localStorage.setItem('originDateTime', luxon.DateTime.fromISO("2023-07-17T00:04:00"));
 }
 
+function updateText(date1, date2) {
+    const duration = calculateDuration(date1, date2);
+    const string = generateDurationString(duration);
+    setElementText(string, 'elapsedTime');
+    return duration.seconds;
+}
+
+const durationSeconds = updateText(luxon.DateTime.now(), luxon.DateTime.fromISO(originDateTime));
+setElementText(durationSeconds + ' secondes', 'seconds')
+
+let seconds = durationSeconds;
+
 // Refresh every second
 setInterval(() => {
-    const duration = calculateDuration(luxon.DateTime.now().setLocale('fr'), luxon.DateTime.fromISO(originDateTime));
-    const string = generateDurationString(duration);
-    setElementText(string, "elapsedTime");
+    setElementText(seconds + ' secondes', 'seconds')
+    seconds++;
+    if (seconds > 60) {
+        setElementText(0 + ' secondes', 'seconds')
+        updateText(luxon.DateTime.now(), luxon.DateTime.fromISO(originDateTime));
+        seconds = 0;
+    }
 }, 1000);
-=======
-function calculateDuration(e,t){const n=e.diff(t,["years","months","weeks","days","hours","minutes","seconds"]).values;return n.seconds=Math.round(n.seconds),n}function generateDurationString(e){const{years:t,months:n,weeks:o,days:s,hours:i,minutes:a,seconds:r}=e,u=["an","ans"],m=["semaine","semaines"],$=["jour","jours"],c=["heure","heures"],l=1===t?`${t} ${u[0]} `:t>1?`${t} ${u[1]} `:"",g=1===o?`${o} ${m[0]} `:o>1?`${o} ${m[1]} `:"",d=1===s?`${s} ${$[0]} `:s>1?`${s} ${$[1]} `:"",D=1===i?`${i} ${c[0]} `:i>1?`${i} ${c[1]} `:"",T=`${l}${n} mois ${g}${d}${D}${a} minutes ${r} secondes`;return T}function setElementText(e,t){const n=document.getElementById(t);n.textContent=e}const originDateTime=localStorage.getItem("originDateTime");originDateTime||localStorage.setItem("originDateTime",luxon.DateTime.fromISO("2023-07-17T00:04:00.000+02:00")),setInterval(()=>{const e=calculateDuration(luxon.DateTime.now(),luxon.DateTime.fromISO(originDateTime)),t=generateDurationString(e);setElementText(t,"elapsedTime")},1e3);
->>>>>>> ae9167e7ef35a4be20206eb161653cc958ef7044:script.js
+
+let weeks = calculateWeekDuration(luxon.DateTime.now(), luxon.DateTime.fromISO(originDateTime));
+let weeksString = generateDurationString(weeks, true);
+setElementText(weeksString, "weeksTime");
